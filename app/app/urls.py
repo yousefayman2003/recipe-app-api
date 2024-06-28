d49ec2503467c8d79c9b82281989fc57f5981046
yousef ayman <yousefayman20003@gmail.com>
2024-06-28 22:49:20 +0300

    Create user Create endpoint

--- app/app/urls.py
+++ app/app/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from drf_spectacular.views import (
+from drf_spectacular.views import (  # type: ignore
     SpectacularAPIView,
     SpectacularSwaggerView,
 )
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,5 +27,6 @@ urlpatterns = [
         'api/docs/',
         SpectacularSwaggerView.as_view(url_name='api-schema'),
         name='api-docs'
-    )
+    ),
+    path('api/user/', include('user.urls'))
 ]
