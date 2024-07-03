2590e043a697d529ab82d6ed14d56dea0285953b
yousef ayman <yousefayman20003@gmail.com>
2024-07-03 08:59:28 +0300

    Created Ingredient Serializer & get endpoint

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -5,7 +5,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Recipe, Tag
+from core.models import Recipe, Tag, Ingredient
 from recipe import serializers
 
 
@@ -44,4 +44,16 @@ class TagViewSet(mixins.DestroyModelMixin,
 
     def get_queryset(self):
         """Retrieve tags for authenicated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+
+class IngredientViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+    """Manage ingredients in the database."""
+    serializer_class = serializers.IngredientSerializer
+    queryset = Ingredient.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        """Retrieve ingredients for authenicated user."""
         return self.queryset.filter(user=self.request.user).order_by('-name')
